c8efc3bae6c8a422295318f695cea9c1354bbd8c
Igor Viveiros Melo Souza <igor.viveiros@sonic.speed.dcc.ufmg.br>
2026-07-02 17:20:02 -0300

    Corrige o comparativo, incorporando o benchmark

--- simulacoes/gera_comparacoes.sh
+++ simulacoes/gera_comparacoes.sh
@@ -3,7 +3,7 @@ cd /sonic_home/igor.viveiros/paralelo
 source /sonic_home/igor.viveiros/py310/bin/activate
 
 python utils/comparar_simulacoes_financeiro.py \
-  --root simulacoes/meu_multi_lb_predlen_todos_ativos \
-  --output simulacoes/meu_multi_lb_predlen_todos_ativos/comparativo_metricas.csv \
-  --long_output simulacoes/meu_multi_lb_predlen_todos_ativos/comparativo_metricas_long.csv \
+  --root simulacoes/tfb_multi_lb_predlen_carteiras \
+  --output simulacoes/tfb_multi_lb_predlen_carteiras/comparativo_metricas_com_bench.csv \
+  --long_output simulacoes/tfb_multi_lb_predlen_carteiras/comparativo_metricas_long_com_bench.csv \
   --pred_len \
